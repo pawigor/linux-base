c003fd34c0b9f2ff8fc23557ab064e2d60fcc6e1
Igor Pawlichenko <pawigor@gmail.com>
2016-09-13 01:07:37 +1000

    6

--- 1/main.c
+++ 1/main.c
@@ -1,6 +1,28 @@
-#include "hello.h"
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+
+void (*hello_message)(const char *);
+
+bool init_library() {
+	void *hdl = dlopen("./libhello.so", RTLD_LAZY);
+	if (NULL == hdl){
+		return false;
+	}
+
+	hello_message = (void (*)(const char*))dlsym(hdl,"hello_message");
+
+	if (NULL == hello_message)
+		return false;
+}
 
 int main(){
-	hello_message("Игорь");
+	if(init_library()){
+		hello_message("Игорь");
+	}else{
+		printf("Error");
+	}
 	return 0;
 }
